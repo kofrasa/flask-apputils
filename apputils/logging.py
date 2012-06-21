70ad1697e85620ca0631cce04e1d3207f45cfb45
Francis Asante <kofrasa@gmail.com>
2012-06-21 11:41:19 +0000

    reimplemented log functions

--- apputils/logging.py
+++ apputils/logging.py
@@ -3,27 +3,14 @@
 
 # convenience methods for accessing app.logger
 
-def _log(level, msg, *args, **kwargs):
-    from flask.globals import current_app as app
-    log = {
-        'debug': app.logger.debug,
-        'error': app.logger.error,
-        'info': app.logger.info,
-        'warn': app.logger.warning
-    }.get(level)(msg, *args, **kwargs)
+from flask.globals import current_app as app
 
+def _create_logger(fn):    
+    def logger(msg, *args, **kwargs):        
+        fn(msg, *args, **kwargs)
+    return logger
 
-def debug(msg, *args, **kwargs):
-    _log('debug', msg, *args, **kwargs)
-    
-
-def warn(msg, *args, **kwargs):
-    _log('warn', msg, *args, **kwargs)
-
-    
-def error(msg, *args, **kwargs):
-    _log('error', msg, *args, **kwargs)
-    
-
-def info(msg, *args, **kwargs):
-    _log('info', msg, *args, **kwargs)
+debug = _create_logger(app.logger.debug)
+error = _create_logger(app.logger.error)
+warn = _create_logger(app.logger.warning)
+info = _create_logger(app.logger.info)
