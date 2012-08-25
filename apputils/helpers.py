bd4fb4e81b9f21859664432f950925f36e585d55
Francis Asante <kofrasa@gmail.com>
2012-08-25 16:35:47 +0000

    renamed and improved *_tag functions implementation

--- apputils/helpers.py
+++ apputils/helpers.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import g, url_for, get_flashed_messages
-from flask.helpers import send_file
 from flaskext.htmlbuilder import html
 
 __all__ = [
     'static',
     'get_flash',
-    'image_tag',
-    'js_include_tag',
-    'css_link_tag',
     'link_to',
+    'image_tag',
+    'style_tag',
+    'script_tag',
 ]
 
 
@@ -24,7 +23,7 @@ def get_flash(category=None, sep='\n'):
     messages = get_flashed_messages(with_categories=True)
     return sep.join([m for k,m in messages if k == category])
 
-    
+
 def link_to(text, endpoint, **kwargs):
     try:
         url = url_for(endpoint)
@@ -32,33 +31,57 @@ def link_to(text, endpoint, **kwargs):
     except:
         pass
     return unicode(html.a(href=endpoint, **kwargs)(text))
-    
-    
-def css_link_tag(filename, **kwargs):
-    from flask.globals import current_app as app
-    
-    try:
-        filepath = 'css/' +  filename + '.css'
-        rel = 'stylesheet'
-        path = static(filepath)        
-        fp = app.send_static_file(filepath)
-        fp.close()
-    except:
-        path = static('css/' +  filename + '.less')
-        rel = 'stylesheet/less'
 
-        
+
+def style_tag(filename, **kwargs):
+    """Creates link tag to CSS or LESS stylesheet file.
+    If no extension is given on the file, CSS is used by default.
+    To send a `.less` file the extension must be explicitly set.
+
+    By default files are sent from the 'css' subdirectory in the static folder
+    To link to files outside of that folder, specify an absolute path such as,
+    '/twitter/css/bootstrap.css'. The root folder is assumed to be under
+    the static folder and the correct path is generated accordingly.
+
+    '/static/twitter/css/boostrap.css'
+    """
+    # support third-party libraries by using absolute path from static folder
+    if filename.startswith('/'):
+        filename = ''.join(filename[1:])
+    else:
+        filename = 'css/' +  filename
+
+    if filename.endswith('.less'):
+        rel = 'stylesheet/less'
+    else:
+        rel = 'stylesheet'
+        if not filename.endswith('.css'):
+            filename += '.css'
+
     for k in ('rel','type','href'):
         kwargs.pop(k,None)
-        
-    return unicode(html.link(rel=rel, type="text/css", href=path, **kwargs))
 
-    
-def js_include_tag(filename, **kwargs):
+    return unicode(html.link(rel=rel, type="text/css", href=static(filename), **kwargs))
+
+
+def script_tag(filename, **kwargs):
+    # support third-party libraries by using absolute path from static folder
+    if filename.startswith('/'):
+        filename = ''.join(filename[1:])
+    else:
+        filename = 'js/' +  filename
+    if not filename.endswith('.js'):
+        filename += '.js'
+
     return unicode(html.script(type="text/javascript",
-                               src=static('js/'+filename+'.js'), **kwargs)())
+                               src=static(filename), **kwargs)())
 
-    
+
 def image_tag(filename, **kwargs):
-    return unicode(html.img(src=static('img/' + filename), **kwargs))
-    
+    # support third-party libraries by using absolute path from static folder
+    if filename.startswith('/'):
+        filename = ''.join(filename[1:])
+    else:
+        filename = 'img/' +  filename
+
+    return unicode(html.img(src=static(filename), **kwargs))
